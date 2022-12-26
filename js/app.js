8fb10174c9d0c22d1e7d01a070ac6e425e4e44d3
Enrique Jimenez <xavier.jimz@icloud.com>
2022-12-25 20:47:43 -0600

    First Commit
    Started project - 12/25/22

---
+++ js/app.js
@@ -0,0 +1,20 @@
+/*Creating arrays to hold the quiz questions and answers. */
+const questions = [
+
+]
+
+const answers = [
+
+]
+
+//getting our submit button reference and making it display new questions once answer has been clicked. 
+const submitButton = document.getElementById('submit-btn')
+submitButton.addEventListener('click', displayQuestion)
+
+function displayQuestion() {
+    console.log('displaying next question')
+}
+
+function selectAnswer() {
+
+}
